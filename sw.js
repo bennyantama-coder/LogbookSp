d385336b749bbd5ed293cbc4e9aaebbb9daa60c6
bennyantama-coder <bennyantama@gmail.com>
2026-05-13 01:30:34 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,4 @@
-const cacheName = 'logbook-v11-fixed';
-const assets = [
-  './', 
-  './LogbookV5_fixed.html', 
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+const cacheName = 'logbook-v12';
+const assets = ['./', './index.html', './manifest.json', 'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js'];
+self.addEventListener('install', e => e.waitUntil(caches.open(cacheName).then(c => c.addAll(assets))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
